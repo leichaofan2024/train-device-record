fafe1c07de739e3a2c64572508821f2db41e434e
leichaofan <leichaofan2024@gmail.com>
2017-06-13 13:27:37 +0800

    add stations CRUD

--- app/controllers/stations_controller.rb
+++ app/controllers/stations_controller.rb
@@ -1,2 +1,47 @@
 class StationsController < ApplicationController
+  def index
+    @stations = Station.all
+  end
+
+  def new
+    @station = Station.new
+  end
+
+  def show
+    @station = Station.find(params[:id])
+  end
+
+  def edit
+    @station = Station.find(params[:id])
+  end
+
+  def create
+    @station = Station.new(station_params)
+    if @station.save
+      redirect_to stations_path
+    else
+      render :new
+    end
+  end
+
+  def update
+    @station = Station.find(params[:id])
+    if @station.update(station_params)
+      redirect_to stations_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @station = Station.find(params[:id])
+    @station.destroy
+    redirect_to stations_path
+  end
+
+  private
+
+  def station_params
+    params.require(:station).permit(:name)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root "welcome#index"
+  root "stations#index"
   resources :stations
-  resources :factories 
+  resources :factories
   resources :equipment_categories do
      resources :transformers
   end
