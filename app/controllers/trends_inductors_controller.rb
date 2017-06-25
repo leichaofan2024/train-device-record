b994576536f28f1e6ed9a9c9bfb4dd466c8bf051
leichaofan <leichaofan2024@gmail.com>
2017-06-25 16:13:34 +0800

    complete trends_inductor

---
+++ app/controllers/trends_inductors_controller.rb
@@ -0,0 +1,59 @@
+class TrendsInductorsController < ApplicationController
+  def index
+    @station = Station.find(params[:station_id])
+    @trends_inductors = TrendsInductor.all.where(station_id: params[:station_id],equipment_category_id: params[:equipment_category_id])
+  end
+
+  def show
+    @equipment_category = EquipmentCategory.find(params[:equipment_category_id])
+    @trends_inductor = TrendsInductor.find(params[:id])
+  end
+
+  def new
+    @equipment_category = EquipmentCategory.find(params[:equipment_category_id])
+    @trends_inductor = TrendsInductor.new
+  end
+
+  def create
+    @trends_inductor = TrendsInductor.new(trends_inductor_params)
+    @equipment_category = EquipmentCategory.find(params[:equipment_category_id])
+
+    @trends_inductor.equipment_category_id = @equipment_category.id
+    if @trends_inductor.save
+      redirect_to station_path(current_station)
+    end
+  end
+
+  def edit
+    @equipment_category = EquipmentCategory.find(params[:equipment_category_id])
+    @trends_inductor = TrendsInductor.find(params[:id])
+  end
+
+  def update
+    @equipment_category = EquipmentCategory.find(params[:equipment_category_id])
+    @trends_inductor = TrendsInductor.find(params[:id])
+    @trends_inductor.update(trends_inductor_params)
+    redirect_to equipment_category_trends_inductor_path(@equipment_category,@trends_inductor)
+  end
+
+  private
+
+  def trends_inductor_params
+    params.require(:trends_inductor).permit(
+                                          :station_id,
+                                          :equipment_category_id,    #
+                                          :factory_id,               #
+                                          :running_number ,           #运行编号
+                                          :category ,                #类型
+                                          :standard ,                 #规格型号
+                                          :count ,                #数量
+                                          :rated_voltage,             #额定电压
+                                          :rated_current,             #额定电流
+                                          :capacity ,                    #容量
+                                          :manufacture_number ,       #出厂序号
+                                          :manufacture_date ,         #出厂日期
+                                          :commissioning_date ,       #投运日期
+                                          :remark ,                   #备注
+                                         )
+  end
+end
---
+++ app/helpers/trends_inductors_helper.rb
@@ -0,0 +1,2 @@
+module TrendsInductorsHelper
+end
